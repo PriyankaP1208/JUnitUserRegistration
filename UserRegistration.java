c5af41a20c560867bdbc5b503358b8812f662860
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-12-27 20:31:41 +0530

    Added:Enter First valid name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    Scanner sc=new Scanner(System.in);
+    public String Name()
+    {
+        String fname;
+        System.out.println("Enter first name:");
+        fname=sc.nextLine();
+        if(Pattern.matches("[A-Z][a-z]{3,}",fname))
+        {
+            System.out.println("Valid First Name");
+            return "HAPPY";
+        }
+        else
+        {
+            System.out.println("Invalid First Name");
+            return "SAD";
+        }
+    }
 }
